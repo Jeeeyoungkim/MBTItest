82aba552e17591f565be28731f89f1d54cb34e3c
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-05-23 01:54:08 +0900

    add KakaoShare

--- js/index.js
+++ js/index.js
@@ -45,8 +45,36 @@ function next() {
   }
 }
 
-//URL 복사 기능
-function shareURLButton() {}
+//URL 복사 기능 - 구글링
+function shareURL() {
+  const dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+
+  const url = window.document.location.href;
+  dummy.value = url;
+
+  navigator.clipboard
+    .writeText(dummy.value)
+    .then(() => {
+      alert("복사가 완료되었습니다");
+    })
+    .catch((err) => {
+      console.log("err:", err);
+    });
+
+  document.body.removeChild(dummy);
+}
 
 //카카오톡으로 공유하기
-function shareKakao() {}
+
+function shareMessage() {
+  Kakao.Share.sendDefault({
+    objectType: "text",
+    text: "기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.",
+    link: {
+      // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+      mobileWebUrl: "https://main--jocular-kulfi-3929a7.netlify.app",
+      webUrl: "https://main--jocular-kulfi-3929a7.netlify.app",
+    },
+  });
+}
